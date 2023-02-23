34a8d31c6c4564906d8897ecdcb1210de0deac3d
Jesse Braham <jesse@beta7.io>
2023-02-23 09:07:23 -0800

    Add support for the ESP32-C6

--- build.rs
+++ build.rs
@@ -1,13 +1,41 @@
 fn main() {
+    // Ensure that only a single chip is specified.
     let chip_features = [
         cfg!(feature = "esp32"),
         cfg!(feature = "esp32c2"),
         cfg!(feature = "esp32c3"),
+        cfg!(feature = "esp32c6"),
         cfg!(feature = "esp32s2"),
         cfg!(feature = "esp32s3"),
     ];
 
-    if chip_features.iter().filter(|&&f| f).count() != 1 {
-        panic!("Exactly one chip must be enabled via its cargo feature");
+    match chip_features.iter().filter(|&&f| f).count() {
+        1 => {}
+        n => panic!("Exactly 1 chip must be enabled via its Cargo feature, {n} provided"),
+    };
+
+    // Ensure that only a single communication method is specified.
+    let method_features = [
+        cfg!(feature = "print-jtag-serial"),
+        cfg!(feature = "print-rtt"),
+        cfg!(feature = "print-uart"),
+    ];
+
+    match method_features.iter().filter(|&&f| f).count() {
+        1 => {}
+        n => panic!(
+            "Exactly 1 communication method must be enabled via its Cargo feature, {n} provided"
+        ),
+    }
+
+    // Ensure that, if the `print-jtag-serial` communication method feature is
+    // enabled, either the `esp32c3`, `esp32c6`, or `esp32s3` chip feature is
+    // enabled.
+    if cfg!(feature = "print-jtag-serial")
+        && !(cfg!(feature = "esp32c3") || cfg!(feature = "esp32c6") || cfg!(feature = "esp32s3"))
+    {
+        panic!(
+            "The `print-jtag-serial` feature is only supported by the ESP32-C3, ESP32-C6, and ESP32-S3"
+        );
     }
 }
